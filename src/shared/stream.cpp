fee503e85dc45049bd23ca4ffc5945f2d872917c
Jenn Wilde <jennowilde@gmail.com>
2017-03-07 00:25:16 -0600

    Swapped unsigned long int to size_t and unsigned int to Sint64 for portability

--- src/shared/stream.cpp
+++ src/shared/stream.cpp
@@ -479,20 +479,20 @@ int listfiles(const char *dir, const char *ext, vector<char *> &files)
 }
 
 #ifndef STANDALONE
-static long int rwopsseek(SDL_RWops *rw, long int pos, int whence)
+static Sint64 rwopsseek(SDL_RWops *rw, Sint64 pos, int whence)
 {
     stream *f = (stream *)rw->hidden.unknown.data1;
     if((!pos && whence==SEEK_CUR) || f->seek(pos, whence)) return (int)f->tell();
     return -1;
 }
 
-static long unsigned int rwopsread(SDL_RWops *rw, void *buf, long unsigned int size, long unsigned int nmemb)
+static size_t rwopsread(SDL_RWops *rw, void *buf, size_t size, size_t nmemb)
 {
     stream *f = (stream *)rw->hidden.unknown.data1;
     return f->read(buf, size*nmemb)/size;
 }
 
-static long unsigned int rwopswrite(SDL_RWops *rw, const void *buf, long unsigned int size, long unsigned int nmemb)
+static size_t rwopswrite(SDL_RWops *rw, const void *buf, size_t size, size_t nmemb)
 {
     stream *f = (stream *)rw->hidden.unknown.data1;
     return f->write(buf, size*nmemb)/size;
